bb45d8a9f06347011533aea5348f6eb66297d7c3
John Flournoy <john_flournoy@fas.harvard.edu>
2020-01-16 13:48:49 -0500

    added option to save return values from processVoxel function as an RDS file for later processing by the user

--- neuropointillist/R/npointWriteOutputFiles.R
+++ neuropointillist/R/npointWriteOutputFiles.R
@@ -7,24 +7,31 @@
 #' @param prefix Prefix for output, to be prepended to outputs
 #' @param results Data returned from the processVoxel function
 #' @param mask A nifti mask that corresponds to the output results
+#' @param rds Save rds file instead of nii? Default is FALSE.
 #' @export
-npointWriteOutputFiles <- function(prefix, results, mask) {
+npointWriteOutputFiles <- function(prefix, results, mask, rds=FALSE) {
    # make sure that any directory specified by prefix exists
     dir <- dirname(prefix)
     if (!dir.exists(dir)) {
         dir.create(dir, recursive=TRUE)
     }
-    # test to see if more than one name was returned 
-    if(is.array(results)) {
-        names <- attributes(results[,1])$names
-        for(i in 1:dim(results)[1]) {
-            statistic <- names[i]
-            outputfilename <- paste(prefix, statistic, ".nii.gz", sep="")
-            npointWriteFile(mask, unlist(results[i,]),outputfilename)
-        }
+    #if user requested rds file output, do that instead of writing nii files.
+    if(rds){
+        outputfilename <- paste(prefix, ".rawresults.rds", sep="")
+        saveRDS(results, file=outputfilename, compress = FALSE)
     } else {
+    # test to see if more than one name was returned 
+        if(is.array(results)) {
+            names <- attributes(results[,1])$names
+            for(i in 1:dim(results)[1]) {
+                statistic <- names[i]
+                outputfilename <- paste(prefix, statistic, ".nii.gz", sep="")
+                npointWriteFile(mask, unlist(results[i,]),outputfilename)
+            }
+        } else {
             statistic <- attributes(results)$names[1]
             outputfilename <- paste(prefix, statistic, ".nii.gz", sep="")
             npointWriteFile(mask, unlist(results),outputfilename)
         }
+    }
 }
